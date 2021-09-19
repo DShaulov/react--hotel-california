449ff8385666c3af8bf2d89553d53c2b084822f4
DShaulov <shaulovl55@gmail.com>
2021-09-19 23:55:42 +0300

    Tried to make animations work

--- client/src/App.js
+++ client/src/App.js
@@ -4,37 +4,22 @@ import Navbar from './components/Navbar/Navbar';
 import AvailabilityContainer from './components/Home-Page/AvailabilityContainer';
 import SidebarNav from './components/Navbar/SidebarNav';
 import ContactUserInfo from './components/Contact-Page/ContactUserInfo';
-import { Spring } from 'react-spring';
 
 function App() {
     const HOME = "Home", CONTACT = "Contact", ROOMS = 'Rooms', MY_BOOKING="My-Booking", MAKE_RESERVATION="Make A Reservation";
-    const [error, setError] = useState(null);
-    const [data, setData] = useState({});
     const [currentPage, setCurrentPage] = useState(HOME);
     const [sidebarVisible, setSidebarVisible] = useState(false);
+    
     const toggleSidebarVisibility = () => {
         setSidebarVisible(!sidebarVisible);
     }
-    useEffect(() => {
-        fetch("/api")
-        .then((res) => res.json())
-        .then(
-            (result) => {
-                setData(result);
-            },
-            (error) => {
-                setError(true);
-            }
-        )
-    });
     const switchPage = (e) => {
         let newPage = e.currentTarget.children[0].innerHTML;
-        setCurrentPage(newPage);
+        setCurrentPage(newPage)
     }
     const homePage = () => {
         return (
             <>
-                <Navbar toggleSidebarVisibility={toggleSidebarVisibility} switchPage={switchPage}/>
                 <div className="app__slogan-container">
                     <h1 className="app__slogan-container__slogan">Make Memories With Us</h1>
                 </div>
@@ -45,7 +30,6 @@ function App() {
     const roomsPage = () => {
         return (
             <>
-                <Navbar toggleSidebarVisibility={toggleSidebarVisibility} switchPage={switchPage}/>
                 <div className="rooms-page-div">
 
                 </div>
@@ -56,7 +40,6 @@ function App() {
     const contactPage = () => {
         return (
             <>
-                <Navbar toggleSidebarVisibility={toggleSidebarVisibility} switchPage={switchPage}/>
                 <div className="contact-page-div">
                     <ContactUserInfo/>
                 </div>
@@ -67,7 +50,6 @@ function App() {
     const myBookingPage = () => {
         return (
             <>
-                <Navbar toggleSidebarVisibility={toggleSidebarVisibility} switchPage={switchPage}/>
                 <div className="my-booking-page-div">
                     <h1>MY BOOKING</h1>
                 </div>
@@ -78,7 +60,6 @@ function App() {
     const makeReservationPage = () => {
         return (
             <>
-                <Navbar toggleSidebarVisibility={toggleSidebarVisibility} switchPage={switchPage}/>
                 <div className="make-reservation-page-div">
                     <h1>Reservation</h1>
                 </div>
@@ -88,17 +69,20 @@ function App() {
     }
     return (
         <div className="app">
+            <Navbar toggleSidebarVisibility={toggleSidebarVisibility} switchPage={switchPage}/>
             {sidebarVisible ? <div className="app__sidebar-div"><SidebarNav/></div>
             : null}
-            {(() => {
-                switch(currentPage) {
-                    case ROOMS : return roomsPage();
-                    case CONTACT : return contactPage();
-                    case MY_BOOKING: return myBookingPage();
-                    case MAKE_RESERVATION: return makeReservationPage();
-                    default: return homePage();
-                }
-            })()}
+            <div className="app__main-page-div">
+                {(() => {
+                    switch(currentPage) {
+                        case ROOMS : return roomsPage();
+                        case CONTACT : return contactPage();
+                        case MY_BOOKING: return myBookingPage();
+                        case MAKE_RESERVATION: return makeReservationPage();
+                        default: return homePage();
+                    }
+                })()}
+            </div>
         </div>
     )
 }
